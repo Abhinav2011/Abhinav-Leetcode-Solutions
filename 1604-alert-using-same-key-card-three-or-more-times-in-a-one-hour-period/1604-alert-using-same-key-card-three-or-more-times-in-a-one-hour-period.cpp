2e53475a2c2331b284cd3d0020a283b02fbe8a20
Abhinav <abhi32eee@gmail.com>
2023-03-26 13:20:09 +0530

    Time: 437 ms (18.32%), Space: 128.6 MB (9.41%) - LeetHub

--- 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cpp
@@ -1,90 +1,39 @@
 class Solution {
 public:
-    string getOneHour(string start){
-        string startAfterOneHour;
-        int hour_a = start[0] - '0';
-        int hour_b = start[1] - '0';
-        if(hour_a == 0 && hour_b == 9){
-            startAfterOneHour += ('1');
-            startAfterOneHour += ('0');
-        }
-        else if(hour_a == 0){
-            hour_b++;
-            startAfterOneHour += '0';
-            startAfterOneHour += to_string(hour_b);
-        }
-        else if(hour_a == 2 && hour_b == 3){
-           startAfterOneHour += ('0' + '0');
-        }
-        else if(hour_a == 2){
-            hour_b++;
-            startAfterOneHour += '2';
-            startAfterOneHour += to_string(hour_b);
-        }
-        else if(hour_a == 1 && hour_b == 0){
-            startAfterOneHour += ('1');
-            startAfterOneHour += ('1');
-        }
-        else if(hour_a == 1 && hour_b == 9){
-            startAfterOneHour += ('2');
-            startAfterOneHour += ('0');
-        }
-        else if(hour_a == 1){
-            hour_b++;
-            startAfterOneHour += '1';
-            startAfterOneHour += to_string(hour_b);
-        }
-        string remaining  = start.substr(2,start.length());
-        startAfterOneHour += remaining;
-        // cout<<startAfterOneHour<<endl;
-        return startAfterOneHour;
-    }
-    bool sendAlert(string name,vector<string>& timeUsed){
-        if(timeUsed.size() == 1 || timeUsed.size() == 2){
-            return false;
-        }
-        
-        int length = timeUsed.size();
-        for(int index=0;index<length;++index){
-            string start = timeUsed[index];
-            string end = getOneHour(start); 
-            int timesCardUsed = 1;
-            for(int current=index+1;current<length;++current){
-                string currentTime = timeUsed[current];
-                if(currentTime >= start && currentTime <= end){                
-                    timesCardUsed++;
-                }
-                else{
-                    break;
-                }
-                if(timesCardUsed >= 3){
-                    return true;
-                }
-            }
+    bool usedWithinLimit(string timeUsedFirst,string timeUsedSecond){
+        int time_a = stoi(timeUsedFirst.substr(0,2)) * 60 + stoi(timeUsedFirst.substr(3));
+        int time_b = stoi(timeUsedSecond.substr(0,2)) * 60 + stoi(timeUsedSecond.substr(3));
+
+        if(time_b - time_a <= 60){
+            return true;
         }
         return false;
     }
     vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
-        map<string,vector<string> > employeeCardUsedData;
-        
+        map<string,vector<string>> workerMap;
         int length = keyName.size();
         for(int index=0;index<length;++index){
-            employeeCardUsedData[keyName[index]].push_back(keyTime[index]);
+            string name = keyName[index];
+            string time = keyTime[index];
+            workerMap[name].push_back(time);
         }
-        vector<string> alertedEmployees;
-        for(auto &employeeData : employeeCardUsedData){
-            string employeeName = employeeData.first;
-            vector<string> timeUsed = employeeData.second;
-            sort(timeUsed.begin(),timeUsed.end());
-            // for(auto &it : timeUsed){
-            //     cout<<it<<" ";
-            // }
-            // cout<<endl;
-            if(sendAlert(employeeName,timeUsed)){
-                alertedEmployees.push_back(employeeName);   
+        vector<string> alertedWorkers;
+        for(auto &values : workerMap){
+            string name = values.first;
+            vector<string> cardUsedTime = values.second;
+            sort(cardUsedTime.begin(),cardUsedTime.end());
+            for(int index=0;index<cardUsedTime.size();++index){
+                if(index + 2 < cardUsedTime.size()){
+                    string timeUsedFirst = cardUsedTime[index];
+                    string timeUsedSecond = cardUsedTime[index + 2];
+                    if(usedWithinLimit(timeUsedFirst,timeUsedSecond)){
+                        alertedWorkers.push_back(name);
+                        break;
+                    }
+                }
             }
         }
-        return alertedEmployees;
+
+        return alertedWorkers;
     }
 };
-
