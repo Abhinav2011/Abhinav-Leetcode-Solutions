ab047b308d7388c5094e1b52e267da136781b763
Abhinav <abhi32eee@gmail.com>
2023-04-29 11:01:28 +0530

    Time: 20 ms (7.17%), Space: 12.8 MB (6.97%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int performOperation(stack<string> &tokenStack,string operand) {
+        int valueA = stoi(tokenStack.top());
+        tokenStack.pop();
+        int valueB = stoi(tokenStack.top());
+        tokenStack.pop();
+        if(operand == "+") {
+            return valueA + valueB;
+        }
+        else if(operand == "-") {
+            return valueB - valueA;
+        }
+        else if(operand == "*") {
+            return valueA * valueB;
+        }
+        else{
+            return valueB / valueA;
+        }
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        int length = tokens.size();
+        stack<string> tokenStack;
+
+        for(int index=0;index<length;++index) {
+            if(tokenStack.empty()) {
+                tokenStack.push(tokens[index]);
+                continue;
+            }
+            if(tokens[index] != "+" && tokens[index] != "-" && tokens[index] != "*" && tokens[index] != "/") {
+                tokenStack.push(tokens[index]);
+            }
+            else {
+                int valueAfterOperation = performOperation(tokenStack,tokens[index]);
+                cout<<valueAfterOperation<<endl;
+                tokenStack.push(to_string(valueAfterOperation));
+            }
+        }
+
+        return stoi(tokenStack.top());        
+    }
+};
