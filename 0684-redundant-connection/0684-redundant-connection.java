018d4170e71724ca07186ec8a51ab7f5984d2710
Abhinav <abhi32eee@gmail.com>
2023-05-21 13:34:53 +0530

    Time: 22 ms (5.2%), Space: 44.2 MB (6.66%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,76 +1,32 @@
 class Solution {
-    public void dfs(int source, Map<Integer,List<Integer>> graph, boolean[] visited) {
-        visited[source] = true;
-        List<Integer> edge = graph.getOrDefault(source, new ArrayList<>());
-        if(edge.size() == 0) return;
-
-        for(int i=0;i<edge.size();++i) {
-            if(!visited[edge.get(i)]) {
-                dfs(edge.get(i), graph, visited);
-            }
-        }
+    public int find(int x, int[] parent) {
+        if(parent[x] == x) {
+            return parent[x];
+        } 
+        return find(parent[x], parent);
     }
 
     public int[] findRedundantConnection(int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int i=0;i<edges.length;++i) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            List<Integer> temp = graph.getOrDefault(u, new ArrayList<>());
-            temp.add(v);
-            graph.put(u, temp);
-            temp = graph.getOrDefault(v, new ArrayList<>());
-            temp.add(u);
-            graph.put(v, temp);
-        }
-        int[] answer = new int[2];
-        for(int i=0;i<edges.length;++i) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            removeEdge(u, v, graph);
-            //perform DFS now
-            boolean[] visited = new boolean[edges.length + 1]; 
-            dfs(1, graph, visited);
-            boolean everyNodeVisited = true;
-            //check if every node has been visited
-            for(int j=1;j<=edges.length;++j) {
-                if(!visited[j]) {
-                    everyNodeVisited = false;
-                    break;
-                }
-            }
-            if(everyNodeVisited) {
-                answer[0] = u;
-                answer[1] = v;
-            }
-            addEdge(u, v, graph);
-        }
-        return answer;
-    }
+        int nodes = edges.length;
+        int[] parent = new int[nodes + 1];
 
-    public void removeEdge(int u, int v, Map<Integer,List<Integer>> graph) {
-        List<Integer> edge = graph.get(u);
-        edge.remove(Integer.valueOf(v));
-        graph.put(u, edge);
-        edge = graph.get(v);
-        edge.remove(Integer.valueOf(u));
-        graph.put(v, edge);
-    }
-
-    public void addEdge(int u, int v, Map<Integer,List<Integer>> graph) {
-        List<Integer> edge = graph.get(u);
-        edge.add(v);
-        graph.put(u, edge);
-        edge = graph.get(v);
-        edge.add(u);
-        graph.put(v, edge);
+        for(int i=1;i<=nodes;++i) {
+            parent[i] = i;
+        }
+        int[] redundantEdge = new int[2];
+        for(int i=0;i<nodes;++i) {
+            int u = find(edges[i][0], parent);
+            int v = find(edges[i][1], parent);
+            System.out.println(u + " " + v);
+            if(u != v) {
+                parent[v] = u;
+            }
+            else{
+                redundantEdge[0] = edges[i][0];
+                redundantEdge[1] = edges[i][1]; 
+                break;
+            }
+        }
+        return redundantEdge;
     }
 }
-
-
-/**
-build graph
-go to every edge...remove that edge and do dfs
-check if every node is visited..if yes add it to answer
-include the removed edge for next dfs
- */
