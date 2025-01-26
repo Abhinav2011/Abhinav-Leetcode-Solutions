253bd398328a44d33f110d79e84d4e2f407e9c24
Abhinav <abhi32eee@gmail.com>
2025-01-26 12:50:53 +0530

    Time: 71 ms (18.12%), Space: 61.9 MB (16.43%) - LeetHub

--- 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.java
+++ 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int[] mostCompetitive(int[] nums, int k) {
+public int[] mostCompetitive(int[] nums, int k) {
         int len = nums.length;
-        Boolean[] canMake = new Boolean[len];
-        Arrays.fill(canMake, false);
-        
+
+        int minValue = Integer.MAX_VALUE;
+        int minValueIndex = -1;
+
         for(int i=0;i<len;++i) {
             int distanceFromHere = len - i;
             if(distanceFromHere >= k) {
-                canMake[i] = true;
-            }
-        }
-        
-        int minValue = Integer.MAX_VALUE;
-        int minValueIndex = -1;
-        
-        for(int i=0;i<len;++i) {
-            if(canMake[i]) {
                 if(nums[i] < minValue) {
                     minValue = nums[i];
                     minValueIndex = i;
                 }
             }
         }
+
         Stack<Integer> stack = new Stack<>();
         for(int start=minValueIndex;start<len;++start) {
             if(stack.isEmpty()) {
                 stack.add(nums[start]);
                 continue;
             }
-            
+
             if(nums[start] < stack.peek()) {
                 int remDistance = len - start;
                 while(nums[start] < stack.peek() && remDistance >= k - stack.size() + 1) {
@@ -37,9 +30,11 @@ class Solution {
             }
             stack.add(nums[start]);
         }
+
         while(stack.size() > k) {
             stack.pop();
         }
+
         int[] answer = new int[stack.size()];
         List<Integer> list = new ArrayList<>();
         while(!stack.isEmpty()) {
