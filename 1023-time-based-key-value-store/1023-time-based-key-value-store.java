f1a4d78a72bbf0f42564485f6220d67ccd5ba9b9
Abhinav <abhi32eee@gmail.com>
2025-01-26 21:14:42 +0530

    Time: 134 ms (82.3%), Space: 119.6 MB (60.5%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,47 +1,49 @@
- class Node {
-        public int timestamp;
-        public String value;
+class Pair {
+    public String key;
+    public int value;
 
-        public Node(int timestamp, String value) {
-            this.timestamp = timestamp;
-            this.value = value;
-        }
+    public Pair(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
 }
- class TimeMap {
-        HashMap<String, List<Node>> map;
-        public TimeMap() {
-            this.map = new HashMap<>();
+class TimeMap {
+    
+    HashMap<String, List<Pair>> map;
+    public TimeMap() {
+        this.map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        map.computeIfAbsent(key, (k) -> new ArrayList<>()).add(new Pair(value, timestamp));
+    }
+
+    public String get(String key, int timestamp) {
+        if(!map.containsKey(key)) {
+            return "";
+        }
+        List<Pair> allValues = map.get(key);
+        int left = 0, right = allValues.size() - 1;
+        int mid, ans = -1;
+
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+
+            if(allValues.get(mid).value <= timestamp) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
 
-        public void set(String key, String value, int timestamp) {
-            map.putIfAbsent(key, new ArrayList<>());
-            List<Node> list = map.get(key);
-            list.add(new Node(timestamp, value));
+        if(ans == -1) {
+            return "";
         }
 
-        public String get(String key, int timestamp) {
-            List<Node> list = map.getOrDefault(key, new ArrayList<>());
-            if(list.isEmpty()) {
-                return "";
-            }
-            int left = 0, right = list.size() - 1;
-            int mid,ans = -1;
-            while(left <= right) {
-                mid = (left + right) / 2;
-                
-                if(timestamp < list.get(mid).timestamp) {
-                    right = mid - 1;
-                }
-                else {
-                    ans = mid;
-                    left = mid + 1;
-                }
-            }
-            if(ans == -1) {
-                return "";
-            }
-            return list.get(ans).value;
-        }
+        return allValues.get(ans).key;
+    }
 }
 
 /**
