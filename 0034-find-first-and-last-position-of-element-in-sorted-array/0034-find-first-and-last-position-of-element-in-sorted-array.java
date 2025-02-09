1af64f69f0df5058ac146e023dd9c9026522aff5
Abhinav <abhi32eee@gmail.com>
2025-02-09 21:31:55 +0530

    Time: 0 ms (100%), Space: 46.1 MB (46.51%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, mid;
-        int[] pos = new int[2];
+    private int leftSearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid, ans = -1;
         while(left <= right) {
             mid = left + (right - left)/2;
 
             if(nums[mid] == target) {
-                int leftEnd = mid, rightEnd = mid;
-                while(leftEnd - 1 >= 0 && nums[leftEnd - 1] == nums[mid]) {
-                    leftEnd--;
+                if(mid == 0) {
+                    ans = mid;
+                    break;
                 }
-                while(rightEnd + 1 < nums.length && nums[rightEnd + 1] == nums[mid]) {
-                    rightEnd++;
+                else if(mid - 1 >= 0 && nums[mid - 1] == nums[mid]) {
+                    ans = mid - 1;
+                    right = mid - 1;
                 }
-                System.out.println("mid = " + mid + " left = " + leftEnd + " right = " + rightEnd);
-                pos[0] = leftEnd;
-                pos[1] = rightEnd;
-                return pos;
-
+                else {
+                    ans = mid;
+                    break;
+                }
             }
             else if(nums[mid] < target) {
                 left = mid + 1;
@@ -26,6 +25,41 @@ class Solution {
                 right = mid - 1;
             }
         }
-        return new int[] {-1,-1};
+        return ans;
+    }
+
+    private int rightSearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid, ans = -1;
+        while(left <= right) {
+            mid = left + (right - left)/2;
+
+            if(nums[mid] == target) {
+                if(mid == nums.length - 1) {
+                    ans = mid;
+                    break;
+                }
+                else if(mid + 1 < nums.length && nums[mid + 1] == nums[mid]) {
+                    ans = mid + 1;
+                    left = mid + 1;
+                }
+                else {
+                    ans = mid;
+                    break;
+                }
+            }
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] pos = new int[2];
+        pos[0] = leftSearch(nums, target);
+        pos[1] = rightSearch(nums, target);
+        return pos;
     }
 }
