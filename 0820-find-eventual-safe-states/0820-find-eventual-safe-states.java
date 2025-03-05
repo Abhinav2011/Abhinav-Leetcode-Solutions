b9dc60883e78f369da7651075d5468e4976ecb05
Abhinav <abhi32eee@gmail.com>
2025-03-05 13:34:27 +0530

    Time: 39 ms (7.44%), Space: 55.8 MB (27.26%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,52 +1,47 @@
 class Solution {
-    private boolean isTerminal(int curr, HashMap<Integer, List<Integer>> map, HashSet<Integer> terminal, HashSet<Integer> visited) {
-        if(terminal.contains(curr)) {
-            return true;
-        }
-        if(visited.contains(curr)) {
-            return false;
-        }
-        visited.add(curr);
-        List<Integer> nei = map.getOrDefault(curr, new ArrayList<>());
-        boolean val = true;
-        for(int i : nei) {
-            val = val && isTerminal(i, map, terminal, visited);
-            if(val) {
-                terminal.add(i);
-            }
-        }
-        return val;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        HashSet<Integer> terminal = new HashSet<>();
         int nodes = graph.length;
-
+        HashMap<Integer, List<Integer>> reverseGraph = new HashMap<>();
+        int[] outgoing = new int[nodes];
         for(int i=0;i<nodes;++i) {
             int[] row = graph[i];
-            if(row.length == 0) {
-                terminal.add(i);
+            for(int k : row) {
+                // System.out.println(i + " " + k);
+                reverseGraph.computeIfAbsent(k, (key) -> new ArrayList<>()).add(i);
             }
-            for (int k : row) {
-                map.computeIfAbsent(i, (key) -> new ArrayList<>()).add(k);
+            outgoing[i] = graph[i].length;
+        }
+
+        
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> terminal = new HashSet<>();
+        for(int i=0;i<nodes;++i) {
+            if(outgoing[i] == 0) {
+                q.offer(i);
             }
         }
-        System.out.println(map);
+        System.out.println(q);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int curr = q.poll();
+                terminal.add(curr);
+                
+                List<Integer> adj = reverseGraph.getOrDefault(curr, new ArrayList<>());
+                for(int nei : adj) {
+                    outgoing[nei]--;
+                    if(outgoing[nei] == 0) {
+                        q.offer(nei);
+                    }
+                }
+            }
+        }
         List<Integer> res = new ArrayList<>();
         for(int i=0;i<nodes;++i) {
             if(terminal.contains(i)) {
                 res.add(i);
-                continue;
-            }
-            HashSet<Integer> visited = new HashSet<>();
-            if(isTerminal(i, map, terminal, visited)) {
-                res.add(i);
-                // terminal.add(i);
             }
         }
-        
         return res;
-
     }
 }
-// 1 0 2 3 
