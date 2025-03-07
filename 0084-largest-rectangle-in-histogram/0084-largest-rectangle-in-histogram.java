d812e458adb36ccc9ac160ad007fea4f3f9e8416
Abhinav <abhi32eee@gmail.com>
2025-03-07 11:41:46 +0530

    Time: 67 ms (63.42%), Space: 56.6 MB (93.84%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,24 +1,36 @@
 class Solution {
+    class Node {
+        int value;
+        int index;
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
     public int largestRectangleArea(int[] heights) {
         int len = heights.length;
-        Stack<Pair<Integer,Integer>> st = new Stack<>();
+        Stack<Node> left = new Stack<>();
         int maxArea = 0;
-        for(int index=0;index<len;++index) {
-            int start = index;
-            while(!st.isEmpty() && st.peek().getValue() > heights[index]) {
-                int currArea = st.peek().getValue() * (index - st.peek().getKey());
-                maxArea = Math.max(maxArea, currArea);
-                start = st.peek().getKey();
-                st.pop();
+        for(int i=0;i<len;++i) {
+            int start = i;
+            if(left.isEmpty()) {
+                left.push(new Node(heights[i], i));
+                continue;
             }
-            st.add(new Pair(start, heights[index]));
+            while(!left.isEmpty() && heights[i] < left.peek().value) {
+                Node node = left.pop();
+                int dis = i - node.index;
+                int area = node.value * dis;
+                start = node.index;
+                maxArea = Math.max(maxArea, area);
+            }
+            left.push(new Node(heights[i], start));
         }
-        while(!st.isEmpty()) {
-            Pair<Integer,Integer> pair = st.pop();
-            int index = pair.getKey();
-            int height = pair.getValue();
-            int currArea = (len - index) * height;
-            maxArea = Math.max(maxArea, currArea);
+        while(!left.isEmpty()) {
+            Node node = left.pop();
+            int dis = len - node.index;
+            int area = node.value * dis;
+            maxArea = Math.max(maxArea, area);
         }
         return maxArea;
     }
